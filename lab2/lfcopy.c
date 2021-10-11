9169d8c4038322b79c60ae9442221383eef898c0
Sarahi Villegas <sg.villegasq@gmail.com>
2021-10-11 14:29:16 +0100

    added makefile

--- lab2/lfcopy.c
+++ lab2/lfcopy.c
@@ -4,7 +4,8 @@
 int readline(char line[], int max);
 int writeline(const char line[]);
 
-int main() {
+int main()
+{
 	char buffer[100];
 	int chars_read = readline(buffer, 100);
 
@@ -15,17 +16,22 @@ int main() {
 
 /* readline: read a line from statndard input, return its length or 0
  */
-int readline(char line[], int max) {
-	if (fgets(line, max, stdin) == NULL) {
-		return 0
-	} else {
+int readline(char line[], int max)
+{
+	if (fgets(line, max, stdin) == NULL)
+	{
+		return 0;
+	}
+	else
+	{
 		return strlen(line);
 	}
 }
 
 /* writeline: write line to standard output, return number of chars written
  */
-int writeline(const char line[]) {
+int writeline(const char line[])
+{
 	fputs(line, stdout);
 	return strlen(line);
 }
