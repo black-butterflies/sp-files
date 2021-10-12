3c7db18616507e12b24a77c1f0143924d884cc8e
Sarahi Villegas <2477179V@student.gla.ac.uk>
2021-10-12 01:43:19 +0100

    Tested task 3 of lab2:  - Modified line_count.c because the behaviour is not as expected  - Modified Makefile to build line_count.c

--- lab2/line_count.c
+++ lab2/line_count.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int lines;
+    int lines = 0;
 
     int buf_size = 100;
     char buffer[buf_size];
 
     do
     {
-        readline(buffer, buf_size);
+        int chars_read = readline(buffer, buf_size);
         writeline(buffer);
 
         lines++;
-    } while (buffer[0] != EOF);
+    } while (chars_read > 0);
 
-    return 0;
+    return lines;
 }
